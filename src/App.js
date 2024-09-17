0e313f9579181c2bb19e3698deccda48d60cac84
Vinilazza <108952051+Vinilazza@users.noreply.github.com>
2024-09-17 13:36:53 -0300

    t

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Error from "./pages/Error/Error";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <div className="App">
@@ -13,8 +14,20 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/about" element={<About />}></Route>
-          <Route path="*" element={<Error/>}></Route>
+          <Route path="*" element={<Error />}></Route>
         </Routes>
+        <ToastContainer
+          position="bottom-left"
+          autoClose={2500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </BrowserRouter>
     </div>
   );
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,36 +1,111 @@
 import styles from "./Home.module.css";
-import { useState} from 'react'
-import useFetch from "react-fetch-hook";
+import { useState } from "react";
+import { PaperClipIcon } from "@heroicons/react/20/solid";
+import { toast } from "react-toastify";
 
 const Home = () => {
+  const [cep, setCep] = useState("");
+  const [results, setResults] = useState("");
 
-  const [cep, setCep] = useState("")
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-   const { data, isLoading, error } = useFetch(`viacep.com.br/ws/89560380/json/`);
+    let cepAlterado = cep.replace(/[^\w\s]/gi, "");
+    console.log(cepAlterado);
+    if (cepAlterado.length === 8) {
+      const res = await fetch(`http://viacep.com.br/ws/${cepAlterado}/json/`)
+        .then((res) => res.json())
+        .catch((err) => console.log(err));
 
-  console.log(data)
-  const handleSubmit = (e) =>{
-    e.preventDefault()
-
-
-
-  }
+      toast.success("Consulta de cep concluida com sucesso!");
+      setResults(res);
+      console.log(res);
+    }
+  };
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <span>Digite o cep: </span>
-          <input type="text" name="cep" onChange={(e) => setCep(e.target.value)}/>
-        </label>
-        <button type="submit">Buscar</button>
-      </form>
-      {cep && (
-        <div>
-        {data && data.map((data)=>(
-          <p>Data: {data.cep}</p>
-        ))}
-        </div>
-      )}
+      <div className="container mx-auto">
+        <form onSubmit={handleSubmit}>
+          <div className="flex items-center mt-5">
+            <label
+              htmlFor="cep"
+              className="block text-sm font-medium leading-6 text-gray-900 me-3"
+            >
+              Digite o cep
+            </label>
+            <div className="me-3">
+              <input
+                name="cep"
+                type="text"
+                autoComplete="cep"
+                onChange={(e) => setCep(e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+            <button
+              type="submit"
+              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Buscar
+            </button>
+          </div>
+        </form>
+        {results && (
+          <div key={results.cep} className={styles.results}>
+            <div>
+              <div className="px-4 sm:px-0">
+                <h3 className="text-base font-semibold leading-7 text-gray-900">
+                  Informações sobre o cep
+                </h3>
+              </div>
+              <div className="mt-6 border-t border-gray-100">
+                <dl className="divide-y divide-gray-100">
+                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                    <dt className="text-sm font-medium leading-6 text-gray-900">
+                      Cep
+                    </dt>
+                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                      {results.cep}
+                    </dd>
+                  </div>
+                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                    <dt className="text-sm font-medium leading-6 text-gray-900">
+                      Estado
+                    </dt>
+                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                      {results.estado} | {results.regiao}
+                    </dd>
+                  </div>
+                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                    <dt className="text-sm font-medium leading-6 text-gray-900">
+                      Bairro
+                    </dt>
+                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                      {results.bairro}
+                    </dd>
+                  </div>
+                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                    <dt className="text-sm font-medium leading-6 text-gray-900">
+                      Cidade
+                    </dt>
+                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                      {results.localidade}
+                    </dd>
+                  </div>
+                  <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                    <dt className="text-sm font-medium leading-6 text-gray-900">
+                      Logradouro
+                    </dt>
+                    <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                      {results.logradouro}
+                    </dd>
+                  </div>
+                </dl>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
