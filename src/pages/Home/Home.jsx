c33daa5b3d30f14ea0135329fb1816396c48d6d6
Vinilazza <108952051+Vinilazza@users.noreply.github.com>
2024-09-18 13:40:43 -0300

    commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import styles from "./Home.module.css";
 import { useState } from "react";
 import { PaperClipIcon } from "@heroicons/react/20/solid";
 import { toast } from "react-toastify";
-import "./Home.css"
+import "./Home.css";
 const Home = () => {
   const [cep, setCep] = useState("");
   const [results, setResults] = useState("");
@@ -27,38 +27,48 @@ const Home = () => {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-          <div className="flex items-center justify-center wrapper  mt-5 h-96">
-            <label
-              htmlFor="cep"
-              className="block text-sm font-medium leading-6 text-gray-900 me-3"
-            >
-              Digite o cep
-            </label>
-            <div className="me-3">
-              <input
-                name="cep"
-                type="text"
-                autoComplete="cep"
-                onChange={(e) => setCep(e.target.value)}
-                value={cep}
-                className="block w-full rounded-md border-0 py-1.5 pl-3  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+        <div className="wrapper bg-cover flex align-center justify-center	bg-center  h-96	">
+          <div className="flex items-center justify-center		">
+            <div className=" wrap    p-7  rounded-xl">
+              <div className="opacity-100">
+                <h1 className="mb-4 text-gray-900">
+                  Encontre o seu cep com facilidade!{" "}
+                </h1>
+                <div className="flex items-center">
+                  <label
+                    htmlFor="cep"
+                    className="block text-sm font-medium leading-6 text-gray-900 me-3"
+                  >
+                    Digite o cep
+                  </label>
+                  <div className="me-3">
+                    <input
+                      name="cep"
+                      type="text"
+                      autoComplete="cep"
+                      onChange={(e) => setCep(e.target.value)}
+                      value={cep}
+                      className="block w-full rounded-md border-0 py-1.5 pl-3  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                  <button
+                    type="submit"
+                    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Buscar
+                  </button>
+                </div>
+              </div>
             </div>
-            <button
-              type="submit"
-              className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Buscar
-            </button>
           </div>
-        </form>
-      <div className="container mx-auto">
-        
+        </div>
+      </form>
+      <div className="container mx-auto mb-5">
         {results && (
           <div key={results.cep} className={styles.results}>
             <div>
               <div className="px-4 sm:px-0">
-                <h3 className="text-base font-semibold leading-7 text-gray-900">
+                <h3 className="text-base font-semibold leading-7 text-gray-900 mt-4 ">
                   Informações sobre o cep
                 </h3>
               </div>
